d94938fbcefdedf1efe3b7598251f9370e302b53
Gabukuro <scatamburlo.gabriel@gmail.com>
2020-08-24 20:06:44 -0300

    Initial commit

---
+++ src/populacaomundial/PopulacaoMundial.java
@@ -0,0 +1,8 @@
+package populacaomundial;
+
+public class PopulacaoMundial {
+    public static void main(String[] args) {
+        System.out.println("Populacao Mundial");
+        System.out.println("7.807.201.724");
+    }
+}
